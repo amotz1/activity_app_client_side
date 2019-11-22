1964407f5f2db2458793e221d5626beee1262b57
amotz taub tabib <amotz.tt@gmail.com>
2019-11-22 20:22:12 +0200

    test of creating react app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class App extends React.Component {
+    render() {
+        return ( <
+            div className = "App" >
+            <h1> hello world!!! </h1> {
+              activitiesJson.map((elem, index) => { < h1 > {
+                    elem.start_time
+                  } < /h1>; })} <
+                  /div>);
+              }
+            }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+            const activitiesJson = [{
+                "start_time": "06:00:00",
+                "end_time": "08:00:00",
+                "activity": "playing chess",
+                "category": "computer",
+                "time_difference": 120
+              },
+              {
+                "start_time": "08:00:00",
+                "end_time": "10:00:00",
+                "activity": "playing basketball",
+                "category": "exercise",
+                "time_difference": 120
+              },
+              {
+                "start_time": "10:00:00",
+                "end_time": "13:00:00",
+                "activity": "talking with gal",
+                "category": "socializing",
+                "time_difference": 180
+              }
+            ]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+            ReactDOM.render( < App / > , document.getElementById('root'));
+
+            // If you want your app to work offline and load faster, you can change
+            // unregister() to register() below. Note this comes with some pitfalls.
+            // Learn more about service workers: https://bit.ly/CRA-PWA
